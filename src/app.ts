b9f39c23bc3ba67bbaa7746da2301d648dfadb17
zhangjunpeng <zhangjunpeng@lexue.com>
2021-12-19 17:48:57 +0800

    主子应用通信--没研究个啥

--- src/app.ts
+++ src/app.ts
@@ -49,27 +49,15 @@ import { useState, useEffect } from 'react';
 // });
 // 不能是异步  只能是同步
 export function useQiankunStateForSlave() {
-  const [masterState, setMasterState] = useState<any>({ zjp: 'child' });
-
-  // function getData () {
-  //   return new Promise((resolve, reject) => {
-  //     resolve({ zjp: 'NB' })
-  //   })
-  // }
-  // const res = await getData()
-  // console.log(res, '异步数据')
+  const [masterState, setMasterState] = useState<any>({ str: 'aaa' });
+  const [ablState, setAblState] = useState({ hh: 'kkk' });
   useEffect(() => {
     console.log('子应用更新数据了', masterState);
   }, [masterState]);
-
-  // setTimeout(() => {
-  //   setMasterState({ctt: 'child'})
-  // }, 2000);
-
-  // setMasterState(res)
-  return {
-    masterState,
-    setMasterState,
-  };
-  // console.log('这是啥')
+  // 实际给子应用调用修改state的方法
+  // 传参和实现可以自定义，子应用直接调用setGlobalState是不生效的，所以才需要这个额外的方法，这是一个坑
+  // const newSetGlobalState = (str: any) => { setGlobalState({ str}) }
+  // const newSetAblState = (hh: any) => { setAblState({hh}) }
+  // return { globalState, setGlobalState, newSetGlobalState ,ablState,setAblState,newSetAblState};
+  return { masterState, setMasterState, ablState, setAblState };
 }
